37f03f00c286555108e4619839a55f36a4ac377f
Axel358 <edgar.serrano.9803@gmail.com>
2023-03-12 10:55:54 -0400

    feat: Add applet settings

--- networkmonitor@axel358/applet.js
+++ networkmonitor@axel358/applet.js
@@ -7,6 +7,7 @@ const St = imports.gi.St;
 const MessageTray = imports.ui.messageTray;
 const GTop = imports.gi.GTop;
 const Mainloop = imports.mainloop;
+const Settings = imports.ui.settings;
 
 class NetworkUsageApplet extends Applet.TextApplet {
 
@@ -14,54 +15,61 @@ class NetworkUsageApplet extends Applet.TextApplet {
         super(orientation, panel_height, instance_id);
         this._source = null;
         this._notification = null;
-        this.refresh_interval = 1000
-        
+        this.settings = new Settings.AppletSettings(this, "networkmonitor@axel358", instance_id);
+
+        this.settings.bind("refresh-interval", "refresh_interval", this.on_settings_changed);
+        this.settings.bind("decimal-places", "decimal_places", this.on_settings_changed);
+        this.settings.bind("hide-umbral", "hide_umbral", this.on_settings_changed);
+
         this.netload = new GTop.glibtop_netload();
-        
+
         try {
             let list = new GTop.glibtop_netlist();
             this.devices = GTop.glibtop.get_netlist(list).filter(device => device !== "lo");
-        }catch(e){
-            this._notify(e+"")
-            global.logError(e+"")
+        } catch(e) {
+            this._notify(e + "")
+            global.logError(e + "")
             this.devices = GTop.glibtop.get_netlist(new GTop.glibtop_netlist()).filter(device => device !== "lo");
         }
-        
+
         this.update();
-        
     }
-    
+
+    on_settings_changed() {
+        this.update();
+    }
+
     update() {
-    
-         
         //Gather network traffic on all devices
         let down = 0;
         let up = 0;
-        
+
         for (let i = 0; i < this.devices.length; ++i)
         {
             GTop.glibtop.get_netload(this.netload, this.devices[i]);
             down += this.netload.bytes_in;
             up += this.netload.bytes_out;
         }
-        
+
         this.set_applet_tooltip("Downloaded: " + this.formatBytes(down) + "\nUploaded: " + this.formatBytes(up))
-        
-        
+
         //Get current up and down speed in bytes
         let down_speed = (down - this.last_down) * 1000 / this.refresh_interval
         let up_speed = (up - this.last_up) * 1000 / this.refresh_interval
-        
+        let total_speed = down_speed + up_speed
+
         //Update last up and down
         this.last_down = down
         this.last_up = up
-        
-        this.set_applet_label("\u21f5 " + this.formatBytes(down_speed + up_speed) + "/s");
-       
-        
+
+        if(total_speed > this.hide_umbral)
+            this.set_applet_label("\u21f5 " + this.formatBytes(total_speed, this.decimal_places) + "/s");
+        else
+             this.set_applet_label("\u21f5 ")
+
         this.update_loop_id = Mainloop.timeout_add(this.refresh_interval, Lang.bind(this, this.update));
     }
-    
+
     formatBytes(bytes, decimals = 1) {
         if (!+bytes) return '0 b'
 
@@ -73,7 +81,7 @@ class NetworkUsageApplet extends Applet.TextApplet {
 
         return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
     }
-    
+
     on_applet_removed_from_panel() {
         if (this.update_loop_id > 0) {
             Mainloop.source_remove(this.update_loop_id);
@@ -109,9 +117,7 @@ class NetworkUsageApplet extends Applet.TextApplet {
             this._notification = null;
         });
         this._source.notify(this._notification);
-
     }
-
 }
 
 function main(metadata, orientation, panel_height, instance_id) {
